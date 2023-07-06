b72cb3289338f7200e83aa0466f4b76e7fb4db54
RameesMhd <muhammed.ramees@neopraxis.in>
2023-07-06 18:51:23 +0530

    useSate Notes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,11 +3,16 @@ import './ExpenseItem.css';
 import Card from "../UI/Card";
 import ExpenseDate from './ExpenseDate';
 
-const ExpenseItem = (props)=> {
+const ExpenseItem = (props) => {
+    // UseState must be called directly inside the function
+    // use state can't be called outside of the function
+    // Aloso shoudn't called inside of any nested function
     const [title, setTitle] = useState(props.title);
-
+    // in the above array destructuring
+    // First Element is the current state value
+    // Second Element is a funciton for updating that
     const clickHandler = () => {
-        setTitle('Title Updated');
+        setTitle('Title Updated'); 
         console.log(title);
     }
     return (
