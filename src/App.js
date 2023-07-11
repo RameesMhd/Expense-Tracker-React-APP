55724ce27dfc084b44ca4c1575153d4e6db80f1c
RameesMhd <muhammed.ramees@neopraxis.in>
2023-07-11 17:38:55 +0530

    create a new expense item using dynamic data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: "Expense 1",
-      amount: 111.11,
-      date: new Date(2023, 1, 1)
-    },
-    {
-      id: 'e2',
-      title: "Expense 2",
-      amount: 222.22,
-      date: new Date(2023, 2, 2)
-    },
-    {
-      id: 'e3',
-      title: "Expense 3",
-      amount: 333.33,
-      date: new Date(2023, 3, 3)
-    },
-    {
-      id: 'e4',
-      title: "Expense 4",
-      amount: 444.44,
-      date: new Date(2023, 4, 4)
-    }
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: "Expense 1",
+    amount: 111.11,
+    date: new Date(2023, 1, 1)
+  },
+  {
+    id: 'e2',
+    title: "Expense 2",
+    amount: 222.22,
+    date: new Date(2023, 2, 2)
+  },
+  {
+    id: 'e3',
+    title: "Expense 3",
+    amount: 333.33,
+    date: new Date(2023, 3, 3)
+  },
+  {
+    id: 'e4',
+    title: "Expense 4",
+    amount: 444.44,
+    date: new Date(2023, 4, 4)
+  }
+];
 
-const addExpenseHandler = expense =>{
-  console.log("App JS");
-  console.log(expense);
-}
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+
+  const addExpenseHandler = expense => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
+  }
 
   return (
     <div className="App">
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
