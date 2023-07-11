f173359812072f02427244d9b621aa3fb72af9ee
RameesMhd <muhammed.ramees@neopraxis.in>
2023-07-11 14:50:07 +0530

    created new component ExpensesFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,13 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
 
     return (
         <Card className="expenses">
+            <ExpensesFilter />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
